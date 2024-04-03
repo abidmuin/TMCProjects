3d658d7bcfe53484ed85ac77a666847f771a5f17
abidmuin <muin.739@gmail.com>
2024-04-03 12:58:05 +0600

    Dinosaur done

--- mooc-java-programming-i/part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
+++ mooc-java-programming-i/part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -1,7 +1,10 @@
 
 public class Dinosaur {
 
-    public static void main(String[] args) {
-        // Write your program here
-    }
+	public static void main(String[] args) {
+		// Write your program here
+		System.out.println("Once upon a time");
+		System.out.println("there was");
+		System.out.println("a dinosaur");
+	}
 }
