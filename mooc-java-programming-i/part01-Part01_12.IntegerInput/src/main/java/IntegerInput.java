318243fa3832229904f272706dae9402db394915
Abid Hasan Muin <muin.739@gmail.com>
2024-04-05 06:41:12 +0600

    IntegerInput done

--- mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class IntegerInput {
@@ -7,6 +6,9 @@ public class IntegerInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Give a number:");
 
+        int value = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + value);
     }
 }
