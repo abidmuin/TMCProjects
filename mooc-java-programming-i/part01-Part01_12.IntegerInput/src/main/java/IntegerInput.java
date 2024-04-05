acd1ecca587064da4803b5122ad1b74807d8db5d
Abid Hasan Muin <muin.739@gmail.com>
2024-04-05 06:57:22 +0600

    IntegerInput done

--- mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -7,8 +7,8 @@ public class IntegerInput {
 
         // write your program here
         System.out.println("Give a number:");
-
         int value = Integer.valueOf(scanner.nextLine());
         System.out.println("You gave the number " + value);
+		scanner.close();
     }
 }
