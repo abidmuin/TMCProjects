9cd8ab7bb54ed162197e9d98647d7ca60562381b
Abid Hasan Muin <muin.739@gmail.com>
2024-04-05 06:53:24 +0600

    DoubleInput done

--- mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class DoubleInput {
@@ -7,6 +6,10 @@ public class DoubleInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Give a number:");
+        double value = Double.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + value);
 
+        scanner.close();
     }
 }
