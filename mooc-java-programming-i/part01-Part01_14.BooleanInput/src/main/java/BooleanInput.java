7200c62d74b6cec4ce078be95bc19cc14d2ce375
Abid Hasan Muin <muin.739@gmail.com>
2024-04-05 07:16:22 +0600

    BooleanInput done

--- mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 
 public class BooleanInput {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // write your program here
-
-    }
+		// write your program here
+		System.out.println("Write something:");
+		boolean value = Boolean.valueOf(scanner.nextLine());
+		System.out.println("True or false? " + value);
+		scanner.close();
+	}
 }
