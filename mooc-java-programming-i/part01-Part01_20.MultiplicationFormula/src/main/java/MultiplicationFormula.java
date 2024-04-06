c4fb673f1f05b1a7cccbc54a7ac0aaa9d3d88c49
Abid Hasan Muin <muin.739@gmail.com>
2024-04-06 11:46:58 +0600

    MultiplicationFormula done

--- mooc-java-programming-i/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ mooc-java-programming-i/part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 
 public class MultiplicationFormula {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+		// Write your program here
+		System.out.println("Give the first number:");
+		int firstNumber = Integer.valueOf(scanner.nextLine());
 
-    }
+		System.out.println("Give the second number:");
+		int secondNumber = Integer.valueOf(scanner.nextLine());
+
+		System.out.println(String.valueOf(firstNumber) + " * " + String.valueOf(secondNumber) + " = "
+				+ (firstNumber * secondNumber));
+		scanner.close();
+
+	}
 }
