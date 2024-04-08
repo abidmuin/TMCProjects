d700b8c7b42898b1f2309d02baded37ac8bc9ba2
abidmuin <muin.739@gmail.com>
2024-04-08 17:37:14 +0600

    GradesAndPoints done

--- mooc-java-programming-i/part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ mooc-java-programming-i/part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -3,8 +3,30 @@ import java.util.Scanner;
 
 public class GradesAndPoints {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-    }
+		System.out.println("Give points [0-100]:");
+		int gradePoints = Integer.valueOf(scan.nextLine());
+
+		if (gradePoints < 0) {
+			System.out.println("impossible!");
+		} else if (gradePoints < 50) {
+			System.out.println("failed");
+		} else if (gradePoints < 60) {
+			System.out.println("1");
+		} else if (gradePoints < 70) {
+			System.out.println("2");
+		} else if (gradePoints < 80) {
+			System.out.println("3");
+		} else if (gradePoints < 90) {
+			System.out.println("4");
+		} else if (gradePoints <= 100) {
+			System.out.println("5");
+		} else {
+			System.out.println("incredible!");
+		}
+
+		scan.close();
+	}
 }
