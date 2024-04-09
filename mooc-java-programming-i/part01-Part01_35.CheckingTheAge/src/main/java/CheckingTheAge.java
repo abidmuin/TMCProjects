ea16129ceb75f86364674f795641cef3c6bb0f08
abidmuin <muin.739@gmail.com>
2024-04-09 13:35:12 +0600

    CheckingTheAge done

--- mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 
 public class CheckingTheAge {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-    }
+		System.out.println("How old are you?");
+		Integer age = Integer.valueOf(scan.nextLine());
+
+		if (age >= 0 && age <= 120) {
+			System.out.println("OK");
+		} else {
+			System.out.println("Impossible!");
+		}
+
+		scan.close();
+
+	}
 }
