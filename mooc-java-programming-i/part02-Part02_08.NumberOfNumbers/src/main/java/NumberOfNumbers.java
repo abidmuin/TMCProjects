e7f7c3bc593daf7e437651b2c4ff1690fcbad900
abidmuin <muin.739@gmail.com>
2024-04-17 18:04:01 +0600

    NumberOfNumbers done

--- mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 
 public class NumberOfNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int inputCounter = 0;
 
-    }
+		while (true) {
+			System.out.println("Give a number:");
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				System.out.println("Number of numbers: " + inputCounter);
+				break;
+			} else {
+				inputCounter++;
+			}
+		}
+
+		scanner.close();
+	}
 }
