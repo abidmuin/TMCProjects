f4e1bb28b77241bd5a39483e9c4c0351ca23ffd5
abidmuin <muin.739@gmail.com>
2024-04-17 15:12:36 +0600

    OnlyPositives done

--- mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 
 public class OnlyPositives {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		while (true) {
+			System.out.println("Give a number:");
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				break;
+			} else if (number > 0) {
+				System.out.println(number * number);
+			} else {
+				System.out.println("Unsuitable number");
+			}
+		}
+		scanner.close();
+	}
 }
