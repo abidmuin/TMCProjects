f1d4078c0f90d192853290c93a10310ecc49355a
Abid Hasan Muin <muin.739@gmail.com>
2024-04-17 21:43:42 +0600

    NumberAndSumOfNumbers done

--- mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -3,8 +3,26 @@ import java.util.Scanner;
 
 public class NumberAndSumOfNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		int inputCounter = 0;
+		int result = 0;
+
+		while (true) {
+			System.out.println("Give a number:");
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				System.out.println("Number of numbers: " + inputCounter);
+				System.out.println("Sum of the numbers: " + result);
+				break;
+			} else {
+				inputCounter++;
+				result += number;
+			}
+		}
+
+		scanner.close();
+	}
 }
