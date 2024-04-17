c8ca5e70573380862d6596d22f15b8bd8675f858
abidmuin <muin.739@gmail.com>
2024-04-17 10:19:36 +0600

    SquareRootOfSum done

--- mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 
 public class SquareRootOfSum {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int firstNumber = Integer.valueOf(scanner.nextLine());
+		int secondNumber = Integer.valueOf(scanner.nextLine());
 
-    }
+		double squareRoot = Math.sqrt(firstNumber + secondNumber);
+		System.out.println(squareRoot);
+
+		scanner.close();
+	}
 }
