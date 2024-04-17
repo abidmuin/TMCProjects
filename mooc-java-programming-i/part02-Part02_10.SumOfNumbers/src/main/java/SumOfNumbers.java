eac8d5ed1c3e1a43f9b37275a46c25c9ff04122b
abidmuin <muin.739@gmail.com>
2024-04-17 18:11:00 +0600

    SumOfNumbers done

--- mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 
 public class SumOfNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int result = 0;
 
-    }
+		while (true) {
+			System.out.println("Give a number:");
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				System.out.println("Sum of the numbers: " + result);
+				break;
+			} else {
+				result += number;
+			}
+		}
+
+		scanner.close();
+	}
 }
