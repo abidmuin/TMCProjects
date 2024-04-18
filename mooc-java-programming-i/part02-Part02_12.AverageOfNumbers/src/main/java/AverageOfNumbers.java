31bea27476436aac2abf9f8802897fc4814872c3
Abid Hasan Muin <muin.739@gmail.com>
2024-04-18 06:45:26 +0600

    AverageOfNumbers done

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 
 public class AverageOfNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		double result = 0;
+		int totalInput = 0;
+
+		while (true) {
+			System.out.println("Give a number:");
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				System.out.println("Average of the numbers: " + (result / totalInput));
+				break;
+			} else {
+				totalInput++;
+				result += number;
+			}
+		}
+
+		scanner.close();
+	}
 }
