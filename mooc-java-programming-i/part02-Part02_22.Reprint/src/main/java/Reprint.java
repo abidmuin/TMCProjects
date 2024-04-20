4d471b1e1b9c5464e1fc4ef5c6c420d1127070dd
Abid Hasan Muin <muin.739@gmail.com>
2024-04-21 00:33:58 +0600

    Reprint done

--- mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 
 public class Reprint {
 
-    public static void main(String[] args) {
-        // ask the user for how many times should the text be printed
-        // then call the printText-method multiple times with a while-loop
-        
-        Scanner scanner = new Scanner(System.in);
-    }
-    
-    // Don't change the next line that defines the method!
-    // (We aren't giving a method a parameter yet)
-    public static void printText() {
-        // write some code here
-    }
+	public static void main(String[] args) {
+		// ask the user for how many times should the text be printed
+		// then call the printText-method multiple times with a while-loop
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("How many times?");
+		int repeatCounter = Integer.valueOf(scanner.nextLine());
+
+		while (repeatCounter > 0) {
+			printText();
+			repeatCounter--;
+		}
+		scanner.close();
+	}
+
+	// Don't change the next line that defines the method!
+	// (We aren't giving a method a parameter yet)
+	public static void printText() {
+		// write some code here
+		System.out.println("In a hole in the ground there lived a method");
+	}
 }
