76a087284c08afa8fe58039613b71bff8a108a44
Abid Hasan Muin <muin.739@gmail.com>
2024-04-20 14:08:21 +0600

    FromWhereToWhere done

--- mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 
 public class FromWhereToWhere {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-    }
+		// Write your program here
+		System.out.println("Where to?");
+		int end = Integer.valueOf(scanner.nextLine());
+
+		System.out.println("Where from?");
+		int start = Integer.valueOf(scanner.nextLine());
+
+		while (start <= end) {
+			System.out.println(start);
+			start++;
+		}
+
+		scanner.close();
+	}
 }
