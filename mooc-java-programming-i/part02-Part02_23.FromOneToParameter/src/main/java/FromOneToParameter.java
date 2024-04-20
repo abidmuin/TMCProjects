024f2ea466dd5756ec46e9e43a177e85bf608b64
Abid Hasan Muin <muin.739@gmail.com>
2024-04-21 00:49:00 +0600

    FromOneToParameter done

--- mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,14 @@
 
-
 public class FromOneToParameter {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		printUntilNumber(1);
+	}
 
-    }
+	private static void printUntilNumber(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+	}
 
 }
