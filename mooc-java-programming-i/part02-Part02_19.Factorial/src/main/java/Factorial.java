41d96c903be3974db3426a34c3097f7f737abc1d
Abid Hasan Muin <muin.739@gmail.com>
2024-04-20 19:14:41 +0600

    Factorial done

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -2,9 +2,23 @@
 import java.util.Scanner;
 
 public class Factorial {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+		System.out.println("Give a number: ");
+		int number = Integer.valueOf(scanner.nextLine());
 
-    }
+		int result = calculateFactorial(number);
+		System.out.println("Factorial: " + result);
+
+		scanner.close();
+	}
+
+	private static int calculateFactorial(Integer n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+
+		return n * calculateFactorial(n - 1);
+	}
 }
