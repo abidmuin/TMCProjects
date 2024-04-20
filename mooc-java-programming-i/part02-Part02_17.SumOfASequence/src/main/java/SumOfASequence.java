0fa006b67f7bef57ec08ec82659db80cf217f32b
Abid Hasan Muin <muin.739@gmail.com>
2024-04-20 14:26:11 +0600

    SumOfASequence done

--- mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 
 public class SumOfASequence {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		int number = Integer.valueOf(scanner.nextLine());
+		int result = (number * (number + 1)) / 2;
+
+		System.out.println(result);
+
+		scanner.close();
+	}
 }
