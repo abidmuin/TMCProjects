514cce41f098f771efc097cbe6c0d47215ab665c
Abid Hasan Muin <muin.739@gmail.com>
2024-04-21 01:03:04 +0600

    Division done

--- mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,15 +1,18 @@
 
-
 public class Division {
 
-    public static void main(String[] args) {
-        
-        // Once you have implemented the division method, you can
-        // try it out here. Fopr example division(3,5);
-        // should print "0.6"
+	public static void main(String[] args) {
 
-        // division(3, 5);
-    }
+		// Once you have implemented the division method, you can
+		// try it out here. For example division(3,5);
+		// should print "0.6"
 
-    // implement the method here
+		division(3, 5);
+	}
+
+	// implement the method here
+	public static void division(int numerator, int denominator) {
+		double result = (numerator * 1.0) / denominator;
+		System.out.println(result);
+	}
 }
