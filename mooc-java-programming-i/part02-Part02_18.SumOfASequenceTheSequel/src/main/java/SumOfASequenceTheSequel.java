9d0c2ba254f38ce1be91f8c83211764f2b4250ea
Abid Hasan Muin <muin.739@gmail.com>
2024-04-20 18:30:15 +0600

    SumOfASequenceTheSequel done

--- mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -3,8 +3,24 @@ import java.util.Scanner;
 
 public class SumOfASequenceTheSequel {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("First number?");
+		int firstNumber = Integer.valueOf(scanner.nextLine());
 
-    }
+		System.out.println("Last number?");
+		int secondNumber = Integer.valueOf(scanner.nextLine());
+
+		if (firstNumber < secondNumber) {
+			int diff = secondNumber - firstNumber + 1;
+			int result = ((diff) * (firstNumber + secondNumber)) / 2;
+			System.out.println("The sum is: " + result);
+		} else {
+			int diff = firstNumber - secondNumber + 1;
+			int result = ((diff) * (firstNumber + secondNumber)) / 2;
+			System.out.println("The sum is: " + result);
+		}
+
+		scanner.close();
+	}
 }
