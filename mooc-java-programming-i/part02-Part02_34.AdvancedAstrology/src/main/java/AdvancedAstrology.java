279946cf4d75e664ae5a5e980406369d64e64086
Abid Hasan Muin <muin.739@gmail.com>
2024-04-21 21:48:53 +0600

    AdvancedAstrology part2 done

--- mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,29 +1,50 @@
 
 public class AdvancedAstrology {
 
-    public static void printStars(int number) {
-        // part 1 of the exercise
-    }
+	public static void printStars(int number) {
+		// part 1 of the exercise
+		for (int i = 0; i < number; i++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+	}
 
-    public static void printSpaces(int number) {
-        // part 1 of the exercise
-    }
+	public static void printSpaces(int number) {
+		// part 1 of the exercise
+		for (int i = 0; i < number; i++) {
+			System.out.print(" ");
+		}
+	}
 
-    public static void printTriangle(int size) {
-        // part 2 of the exercise
-    }
+	public static void printTriangle(int size) {
+		// part 2 of the exercise
+		int spaceSize = size - 1;
+		int starSize = 1;
 
-    public static void christmasTree(int height) {
-        // part 3 of the exercise
-    }
+		while (starSize <= size) {
+			printSpaces(spaceSize);
+			printStars(starSize);
+			spaceSize--;
+			starSize++;
+		}
+	}
 
-    public static void main(String[] args) {
-        // The tests are not checking the main, so you can modify it freely.
+	public static void christmasTree(int height) {
+		// part 3 of the exercise
+		for (int i = 1; i <= height; i++) {
+			// left side pyramid
+			printSpaces(height - i);
+			printStars(i);
+		}
 
-        printTriangle(5);
-        System.out.println("---");
-        christmasTree(4);
-        System.out.println("---");
-        christmasTree(10);
-    }
+	}
+
+	public static void main(String[] args) {
+		// The tests are not checking the main, so you can modify it freely.
+		printTriangle(5);
+		System.out.println("---");
+		christmasTree(4);
+		System.out.println("---");
+		christmasTree(10);
+	}
 }
