1a18a83ed40cf05bd8b0925282f512a353837bf1
Abid Hasan Muin <muin.739@gmail.com>
2024-04-21 06:35:34 +0600

    NumberUno done

--- mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 
 public class NumberUno {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		scanner.close();
+	}
+
+	public static int numberUno() {
+		return 1;
+	}
 
 }
