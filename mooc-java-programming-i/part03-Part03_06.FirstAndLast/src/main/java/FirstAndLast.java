e923414b971c2139434921677dfd218a14f8ccb3
Abid Hasan Muin <muin.739@gmail.com>
2024-04-23 23:09:43 +0600

    FirstAndLast done

--- mooc-java-programming-i/part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
+++ mooc-java-programming-i/part03-Part03_06.FirstAndLast/src/main/java/FirstAndLast.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class FirstAndLast {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
-                break;
-            }
+		ArrayList<String> list = new ArrayList<>();
+		while (true) {
+			String input = scanner.nextLine();
+			if (input.equals("")) {
+				break;
+			}
 
-            list.add(input);
-        }
+			list.add(input);
+		}
 
-    }
+		System.out.println(list.get(0));
+		System.out.println(list.get(list.size() - 1));
+		scanner.close();
+	}
 }
