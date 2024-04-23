ec23581ff78c7fcb8c560a514f395c2d1eb94391
abidmuin <muin.739@gmail.com>
2024-04-23 12:28:36 +0600

    SecondPlusThird done

--- mooc-java-programming-i/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
+++ mooc-java-programming-i/part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class SecondPlusThird {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
-                break;
-            }
-            
-            numbers.add(number);
-        }
+		ArrayList<Integer> numbers = new ArrayList<>();
+		while (true) {
+			int number = Integer.valueOf(scanner.nextLine());
+			if (number == 0) {
+				break;
+			}
 
-        System.out.println(luvut.get(0));
-    }
+			numbers.add(number);
+		}
+
+		System.out.println(numbers.get(1) + numbers.get(2));
+	}
 }
