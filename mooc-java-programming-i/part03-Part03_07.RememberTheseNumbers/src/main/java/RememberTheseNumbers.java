3ffada793939f5477c8aa5e5150fc2585e290a84
Abid Hasan Muin <muin.739@gmail.com>
2024-04-23 23:13:23 +0600

    RememberTheseNumbers done

--- mooc-java-programming-i/part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
+++ mooc-java-programming-i/part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 
 public class RememberTheseNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int luku = Integer.valueOf(scanner.nextLine());
-            if (luku == -1) {
-                break;
-            }
+		ArrayList<Integer> numbers = new ArrayList<>();
+		while (true) {
+			int luku = Integer.valueOf(scanner.nextLine());
+			if (luku == -1) {
+				break;
+			}
 
-            numbers.add(luku);
-        }
+			numbers.add(luku);
+		}
 
-    }
+		int indexValue = 0;
+		while (indexValue < numbers.size()) {
+			System.out.println(numbers.get(indexValue));
+			indexValue++;
+		}
+
+		scanner.close();
+	}
 }
