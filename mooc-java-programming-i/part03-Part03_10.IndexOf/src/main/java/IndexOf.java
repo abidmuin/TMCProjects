f74299871298f24288055cc4c01fe4aae01b7790
abidmuin <muin.739@gmail.com>
2024-04-25 14:22:01 +0600

    IndexOf done

--- mooc-java-programming-i/part03-Part03_10.IndexOf/src/main/java/IndexOf.java
+++ mooc-java-programming-i/part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class IndexOf {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> list = new ArrayList<>();
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            if (input == -1) {
-                break;
-            }
+		ArrayList<Integer> list = new ArrayList<>();
+		while (true) {
+			int input = Integer.valueOf(scanner.nextLine());
+			if (input == -1) {
+				break;
+			}
 
-            list.add(input);
-        }
+			list.add(input);
+		}
 
-        System.out.println("");
+		System.out.println("");
 
-        // implement here finding the indices of a number
-    }
+		// implement here finding the indices of a number
+		System.out.println("Search for?");
+		int number = Integer.valueOf(scanner.nextLine());
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == number) {
+				System.out.println(number + " is at index " + i);				
+			}
+		}
+
+		scanner.close();
+	}
 }
