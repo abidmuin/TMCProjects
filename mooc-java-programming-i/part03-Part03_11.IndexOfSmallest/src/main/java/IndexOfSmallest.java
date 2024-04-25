6b53fd67466f9898230fb74b0a42ae938242213b
abidmuin <muin.739@gmail.com>
2024-04-25 14:31:39 +0600

    IndexOfSmallest done

--- mooc-java-programming-i/part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
+++ mooc-java-programming-i/part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -4,16 +4,42 @@ import java.util.Scanner;
 
 public class IndexOfSmallest {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // implement here a program that reads user input
-        // until the user enters 9999
-        
-        // after that, the program prints the smallest number
-        // and its index -- the smallest number
-        // might appear multiple times
+		// implement here a program that reads user input
+		// until the user enters 9999
 
-        
-    }
+		ArrayList<Integer> list = new ArrayList<>();
+		int input = 0;
+		int smallest = Integer.MAX_VALUE;
+
+		while (true) {
+			input = Integer.valueOf(scanner.nextLine());
+
+			if (input == 9999) {
+				break;
+			}
+
+			if (input < smallest) {
+				smallest = input;
+			}
+
+			list.add(input);
+		}
+
+		System.out.println("Smallest number: " + smallest);
+
+		// after that, the program prints the smallest number
+		// and its index -- the smallest number
+		// might appear multiple times
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == smallest) {
+				System.out.println("Fount at index: " + i);
+			}
+		}
+
+		scanner.close();
+	}
 }
