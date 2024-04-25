2df372d338f354eab9959daca684b192351a8a03
abidmuin <muin.739@gmail.com>
2024-04-25 11:46:10 +0600

    OnlyTheseNumbers done

--- mooc-java-programming-i/part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
+++ mooc-java-programming-i/part03-Part03_08.OnlyTheseNumbers/src/main/java/OnlyTheseNumbers.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 public class OnlyTheseNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == -1) {
-                break;
-            }
+		ArrayList<Integer> numbers = new ArrayList<>();
+		while (true) {
+			int number = Integer.valueOf(scanner.nextLine());
+			if (number == -1) {
+				break;
+			}
 
-            numbers.add(number);
-        }
+			numbers.add(number);
+		}
 
-    }
+		System.out.println("From Where?");
+		int from = Integer.valueOf(scanner.nextLine());
+
+		System.out.println("To Where?");
+		int to = Integer.valueOf(scanner.nextLine());
+
+		System.out.println(numbers.get(from));
+		System.out.println(numbers.get(to));
+
+		scanner.close();
+	}
 }
