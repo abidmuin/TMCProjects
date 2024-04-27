060633c261a9a09f344fc9b39f73f5164c013c6d
Abid Hasan Muin <muin.739@gmail.com>
2024-04-27 17:14:07 +0600

    OnTheList done

--- mooc-java-programming-i/part03-Part03_14.OnTheList/src/main/java/OnTheList.java
+++ mooc-java-programming-i/part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -4,18 +4,36 @@ import java.util.Scanner;
 
 public class OnTheList {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
-                break;
-            }
+		ArrayList<String> list = new ArrayList<>();
 
-            list.add(input);
-        }
+		while (true) {
+			String input = scanner.nextLine();
+			if (input.equals("")) {
+				break;
+			}
 
-    }
+			list.add(input);
+		}
+
+		System.out.print("Search for? ");
+		String searchString = scanner.nextLine();
+		boolean isFound = false;
+
+		for (String string : list) {
+			if (string.equals(searchString)) {
+				System.out.println(searchString + " was found!");
+				isFound = true;
+				break;
+			}
+		}
+
+		if (!isFound) {
+			System.out.println(searchString + " was not found!");
+		}
+		
+		scanner.close();
+	}
 }
