e2e3ff8d34fca507a120328242d0baab6ca6e8ed
abidmuin <muin.739@gmail.com>
2024-04-28 13:34:48 +0600

    RemoveLast done

--- mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -3,8 +3,26 @@ import java.util.ArrayList;
 
 public class RemoveLast {
 
-    public static void main(String[] args) {
-        // Try your method in here
-    }
+	public static void main(String[] args) {
+		// Try your method in here
+		ArrayList<String> strings = new ArrayList<>();
 
+		strings.add("First");
+		strings.add("Second");
+		strings.add("Third");
+
+		System.out.println(strings);
+
+		removeLast(strings);
+		removeLast(strings);
+
+		System.out.println(strings);
+	}
+
+	public static void removeLast(ArrayList<String> strings) {
+		int stringSize = strings.size();
+		if (stringSize > 0) {
+			strings.remove(stringSize - 1);
+		}
+	}
 }
