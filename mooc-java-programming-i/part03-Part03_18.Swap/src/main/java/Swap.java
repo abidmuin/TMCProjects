7c17df1767da569706217407ec21ec57b513a130
abidmuin <muin.739@gmail.com>
2024-04-28 13:40:22 +0600

    Swap done

--- mooc-java-programming-i/part03-Part03_18.Swap/src/main/java/Swap.java
+++ mooc-java-programming-i/part03-Part03_18.Swap/src/main/java/Swap.java
@@ -3,32 +3,41 @@ import java.util.Scanner;
 
 public class Swap {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] array = new int[5];
-        array[0] = 1;
-        array[1] = 3;
-        array[2] = 5;
-        array[3] = 7;
-        array[4] = 9;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] array = new int[5];
+		array[0] = 1;
+		array[1] = 3;
+		array[2] = 5;
+		array[3] = 7;
+		array[4] = 9;
 
-        int index = 0;
-        while (index < array.length) {
-            System.out.println(array[index]);
-            index++;
-        }
-        System.out.println("");
+		int index = 0;
+		while (index < array.length) {
+			System.out.println(array[index]);
+			index++;
+		}
+		System.out.println("");
 
-        // Implement here
-        // asking for the two indices
-        // and then swapping them
+		// Implement here
+		// asking for the two indices
+		// and then swapping them
 
-        System.out.println("");
-        index = 0;
-        while (index < array.length) {
-            System.out.println(array[index]);
-            index++;
-        }
-    }
+		System.out.println("Give two indices to swap:");
+		int firstIndex = Integer.valueOf(scanner.nextLine());
+		int lastIndex = Integer.valueOf(scanner.nextLine());
+		int swapHelper = 0;
+
+		swapHelper = array[firstIndex];
+		array[firstIndex] = array[lastIndex];
+		array[lastIndex] = swapHelper;
+
+		System.out.println("");
+		index = 0;
+		while (index < array.length) {
+			System.out.println(array[index]);
+			index++;
+		}
+	}
 
 }
