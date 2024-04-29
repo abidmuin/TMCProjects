c7fe4904b7b6e456d64d1ece2dfdadaa221eca9f
Abid Hasan Muin <muin.739@gmail.com>
2024-04-29 20:39:55 +0600

    YourFirstBankTransfer done

--- mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -1,8 +1,16 @@
 
 public class YourFirstBankTransfer {
 
-    public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
-    }
+	public static void main(String[] args) {
+		// Do not touch the code in Account.java
+		// write your program here
+		Account firstAccount = new Account("Matthews account", 1000);
+		Account secondAccount = new Account("My account", 0);
+
+		firstAccount.withdrawal(100);
+		secondAccount.deposit(100);
+
+		System.out.println(firstAccount.toString());
+		System.out.println(secondAccount.toString());
+	}
 }
