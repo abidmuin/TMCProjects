7543f1d5cd9fd02575bcc17c2a91675257d1fd31
abidmuin <muin.739@gmail.com>
2024-04-29 13:01:26 +0600

    AgeOfTheOldest done

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -3,9 +3,28 @@ import java.util.Scanner;
 
 public class AgeOfTheOldest {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		int currentAge = Integer.MIN_VALUE;
+		int maxAge = Integer.MIN_VALUE;
 
-    }
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			currentAge = Integer.parseInt(inputString.split(",")[1]);
+
+			if (currentAge > maxAge) {
+				maxAge = currentAge;
+			}
+		}
+
+		System.out.println(maxAge);
+		scanner.close();
+	}
 }
