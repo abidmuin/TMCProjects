bf51a0b71a9c1b5f42013fef37db646edbd275f6
abidmuin <muin.739@gmail.com>
2024-04-29 14:22:14 +0600

    PersonalDetails done

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -4,9 +4,41 @@ import java.util.Scanner;
 
 public class PersonalDetails {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		int inputCount = 0;
 
-    }
+		int currentNameSize = 0;
+		int maxNameSize = 0;
+		String name = "";
+
+		int currentYear = 0;
+		double totalYears = 0;
+
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			currentYear = Integer.parseInt(inputString.split(",")[1]);
+			totalYears += currentYear;
+			inputCount++;
+
+			currentNameSize = inputString.split(",")[0].length();
+
+			if (currentNameSize > maxNameSize) {
+				maxNameSize = currentNameSize;
+				name = inputString.split(",")[0];
+			}
+		}
+
+		System.out.println("Longest name: " + name);
+		System.out.println("Average of the birth years: " + (totalYears / inputCount));
+
+		scanner.close();
+	}
 }
