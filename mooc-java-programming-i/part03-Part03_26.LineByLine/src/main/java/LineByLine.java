6a49b0556332de588cb9ce45e488ceb30babc42b
abidmuin <muin.739@gmail.com>
2024-04-29 11:21:47 +0600

    LineByLine done

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -3,9 +3,27 @@ import java.util.Scanner;
 
 public class LineByLine {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String userInput = "";
+		String sentence = "";
+		String[] arrayWords;
 
-    }
+		while (true) {
+			userInput = scanner.nextLine();
+
+			if (userInput.isEmpty()) {
+				break;
+			}
+
+			arrayWords = userInput.split(" ");
+
+			for (String word : arrayWords) {
+				System.out.println(word);
+			}
+		}
+
+		scanner.close();
+	}
 }
