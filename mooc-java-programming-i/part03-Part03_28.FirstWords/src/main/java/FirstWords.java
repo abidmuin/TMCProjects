b6cb046885f309e4557de7ad1aa4a1006677e1aa
abidmuin <muin.739@gmail.com>
2024-04-29 12:15:41 +0600

    FirstWords done

--- mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -3,9 +3,26 @@ import java.util.Scanner;
 
 public class FirstWords {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		String[] words;
 
-    }
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			words = inputString.split(" ");
+
+			for (String word : words) {
+				System.out.println(word);
+				break;
+			}
+		}
+		scanner.close();
+	}
 }
