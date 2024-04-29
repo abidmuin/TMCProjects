526ecb9256e68d667a8567197fb3037e575ab579
abidmuin <muin.739@gmail.com>
2024-04-29 12:47:45 +0600

    LastWords done

--- mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 
 public class LastWords {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		String[] words;
 
-    }
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			words = inputString.split(" ");
+
+			System.out.println(words[words.length - 1]);
+		}
+
+		scanner.close();
+	}
 }
