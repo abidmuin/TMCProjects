04a9ba01654859e6b1d637d9a9c43c636fdcd402
abidmuin <muin.739@gmail.com>
2024-04-29 12:04:59 +0600

    AVClub done

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -3,9 +3,27 @@ import java.util.Scanner;
 
 public class AVClub {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		String[] words;
 
-    }
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			words = inputString.split(" ");
+
+			for (String word : words) {
+				if (word.contains("av")) {
+					System.out.println(word);
+				}
+			}
+		}
+		scanner.close();
+	}
 }
