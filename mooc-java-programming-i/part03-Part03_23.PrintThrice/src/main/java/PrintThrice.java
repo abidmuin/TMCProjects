58c17934047cfc00bc2475c20aa7132d420200a4
abidmuin <muin.739@gmail.com>
2024-04-29 10:30:34 +0600

    PrintThrice done

--- mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 
 public class PrintThrice {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+		// Write your program here
+		System.out.println("Give a word:");
+		String word = scanner.nextLine();
 
-    }
+		for (int i = 0; i < 3; i++) {
+			System.out.print(word);
+		}
+
+		scanner.close();
+	}
 }
