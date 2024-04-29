f9fea0204c4e0c2f959be58a967198989f5bd130
Abid Hasan Muin <muin.739@gmail.com>
2024-04-29 20:25:56 +0600

    YourFirstAccount done

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/Account.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/Account.java
@@ -4,28 +4,28 @@
 
 public class Account {
 
-    private double balance;
-    private String owner;
+	private double balance;
+	private String owner;
 
-    public Account(String owner, double balance) {
-        this.balance = balance;
-        this.owner = owner;
-    }
+	public Account(String owner, double balance) {
+		this.balance = balance;
+		this.owner = owner;
+	}
 
-    public void deposit(double amount) {
-        this.balance = this.balance + amount;
-    }
+	public void deposit(double amount) {
+		this.balance = this.balance + amount;
+	}
 
-    public void withdrawal(double amount) {
-        this.balance = this.balance - amount;
-    }
+	public void withdrawal(double amount) {
+		this.balance = this.balance - amount;
+	}
 
-    public double saldo() {
-        return this.balance;
-    }
+	public double balance() {
+		return this.balance;
+	}
 
-    @Override
-    public String toString() {
-        return this.owner + " balance: " + this.balance;
-    }
+	@Override
+	public String toString() {
+		return this.owner + " balance: " + this.balance;
+	}
 }
