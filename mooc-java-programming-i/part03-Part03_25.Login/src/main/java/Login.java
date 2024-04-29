0d941eb52070fab69cf1945c5f288ab160fa4bd3
abidmuin <muin.739@gmail.com>
2024-04-29 10:41:04 +0600

    Login done

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,10 +1,28 @@
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Login {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		HashMap<String, String> credentials = new HashMap<>();
+		credentials.put("alex", "sunshine");
+		credentials.put("emma", "haskell");
+
+		System.out.print("Enter username: ");
+		String username = scanner.nextLine();
+
+		System.out.print("Enter password: ");
+		String password = scanner.nextLine();
+
+		if (password.equals(credentials.get(username))) {
+			System.out.println("You have successfully logged in!");
+		} else {
+			System.out.println("Incorrect username or password!");
+		}
+
+		scanner.close();
+	}
 }
