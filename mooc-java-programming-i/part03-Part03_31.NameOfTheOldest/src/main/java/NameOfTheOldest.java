5d6ec3c96e73a86aa706b90b95162a08d9d0a827
abidmuin <muin.739@gmail.com>
2024-04-29 13:12:06 +0600

    NameOfTheOldest done

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -3,9 +3,31 @@ import java.util.Scanner;
 
 public class NameOfTheOldest {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		String inputString = "";
+		String name = "";
+		int currentAge = Integer.MIN_VALUE;
+		int maxAge = Integer.MIN_VALUE;
 
-    }
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.isEmpty()) {
+				break;
+			}
+
+			currentAge = Integer.parseInt(inputString.split(",")[1]);
+
+			if (currentAge > maxAge) {
+				maxAge = currentAge;
+				name = inputString.split(",")[0];
+			}
+		}
+
+		System.out.println(name);
+
+		scanner.close();
+	}
 }
