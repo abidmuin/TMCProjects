cc9247016daf20eb4ac393390be108cd91751693
Abid Hasan Muin <muin.739@gmail.com>
2024-05-03 07:57:33 +0600

    Items

--- mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class Items {
 
-    public static void main(String[] args) {
-        // implement here your program that uses the class Item
+	public static void main(String[] args) {
+		// implement here your program that uses the class Item
 
-        ArrayList<Item> items = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+		ArrayList<Item> items = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String name = "";
+
+		while (scanner.hasNextLine()) {
+			System.out.println("Name: ");
+			Item item = new Item(scanner.nextLine());
+			items.add(item);
+			System.out.println(item.toString());
+		}
+
+		scanner.close();
+	}
 }
