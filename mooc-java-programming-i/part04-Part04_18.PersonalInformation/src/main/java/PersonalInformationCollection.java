d164b209ca0c9f3378b6e4d6abc9c76c15b08727
Abid Hasan Muin <muin.739@gmail.com>
2024-05-04 10:39:28 +0600

    PersonalInformation

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -4,11 +4,33 @@ import java.util.Scanner;
 
 public class PersonalInformationCollection {
 
-    public static void main(String[] args) {
-        // implement here your program that uses the PersonalInformation class
+	public static void main(String[] args) {
+		// implement here your program that uses the PersonalInformation class
 
-        ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+		ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String firstName = "";
+		String lastName = "";
+		String identificationNumber = "";
+
+		while (true) {
+			firstName = scanner.nextLine();
+
+			if (firstName.isEmpty()) {
+				break;
+			}
+
+			lastName = scanner.nextLine();
+			identificationNumber = scanner.nextLine();
+
+			infoCollection.add(new PersonalInformation(firstName, lastName, identificationNumber));
+		}
+
+		for (PersonalInformation personalInformation : infoCollection) {
+			System.out.println(personalInformation.getFirstName() + " " + personalInformation.getLastName());
+		}
+
+		scanner.close();
+	}
 }
