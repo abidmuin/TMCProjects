a2ff4ba8dc113de8b1642cb0dc0984e8ba650fbb
Abid Hasan Muin <muin.739@gmail.com>
2024-05-04 10:52:31 +0600

    TelevisionProgram

--- mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -3,11 +3,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // implement here your program that uses the TelevisionProgram class
+	public static void main(String[] args) {
+		// implement here your program that uses the TelevisionProgram class
 
-        ArrayList<TelevisionProgram> programs = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+		ArrayList<TelevisionProgram> programs = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String name = "";
+		int duration = 0;
+
+		while (true) {
+			name = scanner.nextLine();
+
+			if (name.isEmpty()) {
+				break;
+			}
+			duration = Integer.valueOf(scanner.nextLine());
+
+			programs.add(new TelevisionProgram(name, duration));
+		}
+
+		System.out.print("Program's maximum duration? ");
+		duration = Integer.valueOf(scanner.nextLine());
+
+		for (TelevisionProgram televisionProgram : programs) {
+			if (televisionProgram.getDuration() <= duration) {
+				System.out.print(televisionProgram.getName() + ", " + televisionProgram.getDuration() + " minutes");
+			}
+		}
+		scanner.close();
+	}
 }
