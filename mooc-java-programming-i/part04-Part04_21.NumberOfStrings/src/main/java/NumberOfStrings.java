551b58653338f8d79adf7ab3a07ea99770bf26ff
Abid Hasan Muin <muin.739@gmail.com>
2024-05-04 16:46:53 +0600

    NumberOfStrings

--- mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -3,8 +3,24 @@ import java.util.Scanner;
 
 public class NumberOfStrings {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String word = "";
+		int inputCount = 0;
+
+		while (true) {
+			word = scanner.nextLine();
+
+			if (word.equals("end")) {
+				break;
+			}
+
+			inputCount++;
+		}
+
+		System.out.println(inputCount);
+
+		scanner.close();
+	}
 }
