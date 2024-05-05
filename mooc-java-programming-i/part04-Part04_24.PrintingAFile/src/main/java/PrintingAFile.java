a1ff929b26e304c5d9e7b648c4bd4d6a8f0fc8b6
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 07:15:49 +0600

    PrintingAFile

--- mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class PrintingAFile {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+		try (Scanner scanner = new Scanner(Paths.get("data.txt"))) {
+			while (scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				System.out.println(row);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
