183fce0c9e242b86d79fc161f4b7a20d8564240f
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 08:44:13 +0600

    PrintingASpecifiedFile

--- mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,11 +1,23 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class PrintingASpecifiedFile {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		System.out.println("Which file should have its contents printed?");
+		String fileName = scanner.nextLine();
+		scanner.close();
+		
+		try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+			while (fileScanner.hasNextLine()) {
+				String row = fileScanner.nextLine();
+				System.out.println(row);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
