1831cd41ef58743cf3f346b90740cf2d74e54e69
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 06:20:39 +0600

    Cubes

--- mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -3,8 +3,24 @@ import java.util.Scanner;
 
 public class Cubes {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		String inputString = "";
+		int number = 0;
+
+		while (true) {
+			inputString = scanner.nextLine();
+
+			if (inputString.equals("end")) {
+				break;
+			}
+
+			number = Integer.valueOf(inputString);
+
+			System.out.println(number * number * number);
+		}
+
+		scanner.close();
+	}
 }
