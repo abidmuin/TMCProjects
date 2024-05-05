ee268a38a3453d7ea3481932e3a4158a39ec5d1d
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 14:41:23 +0600

    GuestListFromAFile

--- mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -1,34 +1,52 @@
 
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GuestListFromAFile {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Name of the file:");
-        String file = scanner.nextLine();
+		System.out.println("Name of the file:");
+		String fileName = scanner.nextLine();
 
-        ArrayList<String> list = new ArrayList<>();
-        // implement reading the file here.
-        System.out.println("");
+		ArrayList<String> list = new ArrayList<>();
+		// implement reading the file here.
 
-        System.out.println("Enter names, an empty line quits.");
-        while (true) {
-            String name = scanner.nextLine();
-            if (name.isEmpty()) {
-                break;
-            }
+		try {
+			File file = new File(fileName);
+			Scanner fileScanner = new Scanner(file);
 
-            if (list.contains(name)) {
-                System.out.println("The name is on the list.");
-            } else {
-                System.out.println("The name is not on the list.");
-            }
-        }
+			while (fileScanner.hasNextLine()) {
+				String name = fileScanner.nextLine();
+				list.add(name);
+			}
 
-        System.out.println("Thank you!");
-    }
+			fileScanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + fileName);
+			System.exit(1);
+		}
+
+		System.out.println("");
+
+		System.out.println("Enter names, an empty line quits.");
+		while (true) {
+			String name = scanner.nextLine();
+			if (name.isEmpty()) {
+				break;
+			}
+
+			if (list.contains(name)) {
+				System.out.println("The name is on the list.");
+			} else {
+				System.out.println("The name is not on the list.");
+			}
+		}
+
+		scanner.close();
+		System.out.println("Thank you!");
+	}
 }
