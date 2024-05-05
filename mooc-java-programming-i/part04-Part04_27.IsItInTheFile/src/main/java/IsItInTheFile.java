714488f9cb2b7be759d2ba6955f6c71544381a48
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 19:20:44 +0600

    IsItInTheFile V1

--- mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -3,44 +3,44 @@ import java.util.Scanner;
 
 public class IsItInTheFile {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Name of the file:");
-        String fileName = scanner.nextLine();
+		System.out.println("Name of the file:");
+		String fileName = scanner.nextLine();
 
-        System.out.println("Search for:");
-        String searchedFor = scanner.nextLine();
+		System.out.println("Search for:");
+		String searchedFor = scanner.nextLine();
 
-        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
-            String row = "";
-            boolean isFound = false;
+		try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+			String row = "";
+			boolean isFound = false;
 
-            while (fileScanner.hasNextLine()) {
-                row = fileScanner.nextLine();
+			while (fileScanner.hasNextLine()) {
+				row = fileScanner.nextLine();
 
-                if (row.isEmpty()) {
-                    break;
-                }
+				if (row.isEmpty()) {
+					break;
+				}
 
-                if (row.equals(searchedFor)) {
-                    isFound = true;
-                    break;
-                }
-            }
+				if (row.equals(searchedFor)) {
+					isFound = true;
+					break;
+				}
+			}
 
-            if (isFound) {names.txt
-                System.out.println("Found!");
-            } else {
-                System.out.println("Not found.");
-            }
+			if (isFound) {
+				System.out.println("Found!");
+			} else {
+				System.out.println("Not found.");
+			}
 
-            fileScanner.close();
-        } catch (Exception e) {
-            System.out.println("Reading the file " + fileName + " failed.");
-            System.exit(1);
-        }
+			fileScanner.close();
+		} catch (Exception e) {
+			System.out.println("Reading the file " + fileName + " failed.");
+			System.exit(1);
+		}
 
-        scanner.close();
-    }
+		scanner.close();
+	}
 }
