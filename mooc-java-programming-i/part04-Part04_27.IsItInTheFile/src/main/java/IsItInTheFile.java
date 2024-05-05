4c2404fe233d1d37005f850ab15d1afc04f3ebc3
Abid Hasan Muin <muin.739@gmail.com>
2024-05-05 19:19:00 +0600

    IsItInTheFile

--- mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,4 +1,3 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -8,10 +7,40 @@ public class IsItInTheFile {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
-        String file = scanner.nextLine();
+        String fileName = scanner.nextLine();
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
 
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            String row = "";
+            boolean isFound = false;
+
+            while (fileScanner.hasNextLine()) {
+                row = fileScanner.nextLine();
+
+                if (row.isEmpty()) {
+                    break;
+                }
+
+                if (row.equals(searchedFor)) {
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (isFound) {names.txt
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+
+            fileScanner.close();
+        } catch (Exception e) {
+            System.out.println("Reading the file " + fileName + " failed.");
+            System.exit(1);
+        }
+
+        scanner.close();
     }
 }
