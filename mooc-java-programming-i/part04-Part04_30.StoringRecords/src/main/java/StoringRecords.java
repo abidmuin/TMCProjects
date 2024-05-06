4c730646dc283ab2da4fc36c42b0cfcde17f3105
abidmuin <muin.739@gmail.com>
2024-05-06 14:23:58 +0600

    StoringRecords

--- mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -5,27 +5,45 @@ import java.util.Scanner;
 
 public class StoringRecords {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-        System.out.println("Filename:");
-        String file = scan.nextLine();
+		System.out.println("Filename:");
+		String file = scan.nextLine();
 
-        ArrayList<Person> records = readRecordsFromFile(file);
-        System.out.println("Persons: " + records.size());
-        System.out.println("Persons:");
-        for (Person person : records) {
-            System.out.println(person);
+		ArrayList<Person> records = readRecordsFromFile(file);
+		System.out.println("Persons: " + records.size());
+		System.out.println("Persons:");
 
-        }
-    }
+		for (Person person : records) {
+			System.out.println(person);
+		}
 
-    public static ArrayList<Person> readRecordsFromFile(String file) {
-        ArrayList<Person> persons = new ArrayList<>();
+		scan.close();
+	}
 
-        // Write here the code for reading from file
-        // and printing the read records
-        return persons;
+	public static ArrayList<Person> readRecordsFromFile(String file) {
+		ArrayList<Person> persons = new ArrayList<>();
 
-    }
+		// Write here the code for reading from file
+		// and printing the read records
+		String name = "";
+		int age = 0;
+		String row = "";
+
+		try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+			while (fileScanner.hasNextLine()) {
+				row = fileScanner.nextLine();
+				name = row.split(",")[0];
+				age = Integer.valueOf(row.split(",")[1]);
+
+				persons.add(new Person(name, age));
+				persons.toString();
+			}
+		} catch (Exception e) {
+			System.out.println("Error file opening: " + e);
+		}
+
+		return persons;
+	}
 }
