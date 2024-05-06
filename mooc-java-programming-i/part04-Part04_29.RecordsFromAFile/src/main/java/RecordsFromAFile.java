89bb31c57383fc3f4c5897ecab649e7cdc1cfdd5
abidmuin <muin.739@gmail.com>
2024-05-06 14:08:30 +0600

    RecordsFromAFile

--- mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 
 public class RecordsFromAFile {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		System.out.println("Name of the file:");
+		String fileName = scanner.nextLine();
+
+		String name = "";
+		String age = "";
+		String row = "";
+
+		try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+			while (fileScanner.hasNextLine()) {
+				row = fileScanner.nextLine();
+				name = row.split(",")[0];
+				age = row.split(",")[1];
+				System.out.println(name + "age: " + age + " years");
+			}
+
+		} catch (Exception e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+
+		scanner.close();
+	}
 }
