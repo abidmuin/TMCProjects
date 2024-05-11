581f3e7abbe637c4cd653894740a72d986b7aff0
Abid Hasan Muin <muin.739@gmail.com>
2024-05-11 18:47:50 +0600

    BiggestPetShop

--- mooc-java-programming-i/part05-Part05_08.BiggestPetShop/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part05-Part05_08.BiggestPetShop/src/main/java/MainProgram.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class MainProgram {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-        Pet hulda = new Pet("Hulda", "Golden retriever");
-        Person leo = new Person("Leo", hulda);
+		Pet hulda = new Pet("Hulda", "Golden retriever");
+		Person leo = new Person("Leo", hulda);
 
-        System.out.println(leo);
-    }
+		System.out.println(leo);
+
+		scan.close();
+	}
 }
