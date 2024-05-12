0a3913bda5f02bff7213bac5a7acf933497c846a
Abid Hasan Muin <muin.739@gmail.com>
2024-05-12 23:06:46 +0600

    Song

--- mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -1,20 +1,41 @@
+import java.util.Objects;
 
 public class Song {
 
-    private String artist;
-    private String name;
-    private int durationInSeconds;
+	private String artist;
+	private String name;
+	private int durationInSeconds;
 
-    public Song(String artist, String name, int durationInSeconds) {
-        this.artist = artist;
-        this.name = name;
-        this.durationInSeconds = durationInSeconds;
-    }
+	public Song(String artist, String name, int durationInSeconds) {
+		this.artist = artist;
+		this.name = name;
+		this.durationInSeconds = durationInSeconds;
+	}
 
-    @Override
-    public String toString() {
-        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
-    }
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
 
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
 
+		Song newSong = (Song) object;
+
+		if (this.durationInSeconds != newSong.durationInSeconds) {
+			return false;
+		}
+
+		if (!Objects.equals(this.artist, newSong.artist)) {
+			return false;
+		}
+
+		return Objects.equals(this.name, newSong.name);
+	}
+
+	@Override
+	public String toString() {
+		return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+	}
 }
