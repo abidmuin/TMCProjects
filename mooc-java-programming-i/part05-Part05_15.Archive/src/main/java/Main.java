2a89f3233e3651fb8034e27c6e27c2d5984a7ff7
Abid Hasan Muin <muin.739@gmail.com>
2024-05-13 11:24:38 +0600

    Archive

--- mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -1,5 +1,5 @@
-
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +7,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String identifier = "";
+        String name = "";
+        Map<String, String> records = new HashMap<>();
 
+        while (scanner.hasNextLine()) {
+            identifier = scanner.nextLine();
+
+            if (identifier.isEmpty()) {
+                break;
+            }
+
+            name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (!records.containsKey(identifier)) {
+                records.put(identifier, name);
+            }
+        }
+
+        for (String key : records.keySet()) {
+            System.out.println(key + ": " + records.get(key));
+        }
+
+        scanner.close();
     }
 }
