1e1670dab3cf18438cca567464e35ab35ab75731
Abid Hasan Muin <muin.739@gmail.com>
2024-05-16 04:59:25 +0600

    PrintingACollection

--- mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -1,10 +1,9 @@
-
 import java.util.ArrayList;
 
 public class SimpleCollection {
 
-    private String name;
-    private ArrayList<String> elements;
+    private final String name;
+    private final ArrayList<String> elements;
 
     public SimpleCollection(String name) {
         this.name = name;
@@ -18,5 +17,27 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    @Override
+    public String toString() {
+        if (this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        }
+
+        int alphabetCount = this.elements.size();
+
+        StringBuilder result = new StringBuilder("The collection " + this.name + " has " + alphabetCount);
+
+        if (alphabetCount == 1) {
+            result.append(" ").append("element:");
+        } else {
+            result.append(" ").append("elements:");
+        }
+
+        for (int i = 0; i < alphabetCount; i++) {
+            result.append("\n").append(this.elements.get(i));
+        }
+
+        return result.toString();
+    }
 }
