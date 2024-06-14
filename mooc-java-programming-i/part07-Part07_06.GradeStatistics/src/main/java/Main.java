b6cd9a863083e61f2e5fc23abebaecc764e52e17
Abid Hasan Muin <muin.739@gmail.com>
2024-06-14 23:15:48 +0600

    GradeStatistics Part01, Part02 done

--- mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -1,12 +1,58 @@
-
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Write your program here -- consider breaking the program into 
-        // multiple classes.
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Write your program here -- consider breaking the program into
+		// multiple classes.
+
+		pointAverage();
+	}
+
+	public static boolean validPoint(int point) {
+		return (point >= 0 && point <= 100);
+	}
+
+	public static boolean passedPoint(int point) {
+		return (point >= 50 && point <= 100);
+	}
+
+	public static void pointAverage() {
+		Scanner scanner = new Scanner(System.in);
+
+		int totalPoints = 0;
+		int counterPoints = 0;
+		double averagePoints = 0;
+
+		int totalPassedPoints = 0;
+		int totalPassedCounter = 0;
+		double averagePassedPoints = 0;
+
+		System.out.println("Enter point totals, -1 stops:");
+
+		while (true) {
+			int points = scanner.nextInt();
+			if (points == -1) {
+				break;
+			}
+
+			if (validPoint(points)) {
+				totalPoints += points;
+				counterPoints++;
+
+				if (passedPoint(points)) {
+					totalPassedPoints += points;
+					totalPassedCounter++;
+				}
+			}
+		}
+
+		averagePoints = totalPoints / counterPoints;
+		averagePassedPoints = totalPassedPoints / totalPassedCounter;
+
+		System.out.println("Point average (all): " + averagePoints);
+		System.out.println("Point average (passing): " + averagePassedPoints);
+	}
 }
