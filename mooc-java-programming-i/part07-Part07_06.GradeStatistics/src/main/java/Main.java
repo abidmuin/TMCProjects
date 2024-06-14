f5c1ea4d90d54b8da1f12b06453b534680597fc8
Abid Hasan Muin <muin.739@gmail.com>
2024-06-15 04:37:11 +0600

    GradeStatistics

--- mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +9,7 @@ public class Main {
 		// Write your program here -- consider breaking the program into
 		// multiple classes.
 
-		pointAverage();
+		pointAverage(scanner);
 	}
 
 	public static boolean validPoint(int point) {
@@ -19,16 +20,18 @@ public class Main {
 		return (point >= 50 && point <= 100);
 	}
 
-	public static void pointAverage() {
-		Scanner scanner = new Scanner(System.in);
-
+	public static void pointAverage(Scanner scanner) {
 		int totalPoints = 0;
 		int counterPoints = 0;
-		double averagePoints = 0;
+		double averagePoints;
 
 		int totalPassedPoints = 0;
 		int totalPassedCounter = 0;
-		double averagePassedPoints = 0;
+		double averagePassedPoints;
+
+		double passPercentage = 0;
+
+		HashMap<Integer, Integer> gradeDistribution = new HashMap<>();
 
 		System.out.println("Enter point totals, -1 stops:");
 
@@ -46,13 +49,61 @@ public class Main {
 					totalPassedPoints += points;
 					totalPassedCounter++;
 				}
+
+				int grade = gradeCalculation(points);
+				if (gradeDistribution.containsKey(grade)) {
+					gradeDistribution.put(grade, gradeDistribution.get(grade) + 1);
+				} else {
+					gradeDistribution.put(grade, 1);
+				}
 			}
 		}
 
-		averagePoints = totalPoints / counterPoints;
-		averagePassedPoints = totalPassedPoints / totalPassedCounter;
+		averagePoints = (double) totalPoints / counterPoints;
 
 		System.out.println("Point average (all): " + averagePoints);
-		System.out.println("Point average (passing): " + averagePassedPoints);
+
+		if (totalPassedCounter > 0) {
+			averagePassedPoints = (double) totalPassedPoints / totalPassedCounter;
+			System.out.println("Point average (passing): " + averagePassedPoints);
+		} else {
+			System.out.println("Point average (passing): -");
+		}
+
+		if (totalPassedCounter > 0) {
+			passPercentage = ((double) totalPassedCounter / counterPoints) * 100;
+			System.out.println("Pass percentage: " + passPercentage);
+		} else {
+			System.out.println("Pass percentage: " + passPercentage);
+		}
+
+		printGradeDistribution(gradeDistribution);
+	}
+
+	public static int gradeCalculation(int point) {
+		if (point < 50) {
+			return 0;
+		} else if (point < 60) {
+			return 1;
+		} else if (point < 70) {
+			return 2;
+		} else if (point < 80) {
+			return 3;
+		} else if (point < 90) {
+			return 4;
+		}
+
+		return 5;
+	}
+
+	public static void printGradeDistribution(HashMap<Integer, Integer> gradeDistribution) {
+		for (int i = 5; i >= 0; i--) {
+			System.out.print(i + ": ");
+			int count = gradeDistribution.getOrDefault(i, 0);
+			for (int j = 0; j < count; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
