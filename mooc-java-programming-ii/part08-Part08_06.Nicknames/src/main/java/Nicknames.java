3069560926de1b27af04ba017ac0d48f785cbc45
Abid Hasan Muin <muin.739@gmail.com>
2024-06-22 05:45:15 +0600

    Nicknames

--- mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -1,10 +1,18 @@
-
 import java.util.HashMap;
+import java.util.Map;
 
 public class Nicknames {
 
-    public static void main(String[] args) {
-        // Do the operations required here!
-    }
+	public static void main(String[] args) {
+		// Do the operations required here!
+		Map<String, String> nicknames = new HashMap<>();
 
+		nicknames.put("matthew", "matt");
+		nicknames.put("michael", "mix");
+		nicknames.put("arthur", "artie");
+
+		for (String nickname : nicknames.keySet()) {
+			System.out.println(nicknames.get(nickname));
+		}
+	}
 }
