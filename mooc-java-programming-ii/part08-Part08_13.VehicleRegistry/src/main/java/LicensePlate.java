77a2cede1cac2b117f0b1de86c4241c7b6a07805
Abid Hasan Muin <muin.739@gmail.com>
2024-06-24 13:00:33 +0600

    VehicleRegistry Part 01 Done

--- mooc-java-programming-ii/part08-Part08_13.VehicleRegistry/src/main/java/LicensePlate.java
+++ mooc-java-programming-ii/part08-Part08_13.VehicleRegistry/src/main/java/LicensePlate.java
@@ -1,22 +1,40 @@
-
 import java.util.Objects;
 
 public class LicensePlate {
-    // don't modify existing parts of this class
+	// don't modify existing parts of this class
 
-    // these instance variables have been defined as final, meaning 
-    // that once set, their value can't be changed
-    private final String liNumber;
-    private final String country;
+	// these instance variables have been defined as final, meaning
+	// that once set, their value can't be changed
+	private final String liNumber;
+	private final String country;
 
-    public LicensePlate(String country, String liNumber) {
-        this.liNumber = liNumber;
-        this.country = country;
-    }
+	public LicensePlate(String country, String liNumber) {
+		this.liNumber = liNumber;
+		this.country = country;
+	}
 
-    @Override
-    public String toString() {
-        return country + " " + liNumber;
-    }
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		
+		if (object == null || getClass() != object.getClass()) {
+			return false;
+		}
+
+		LicensePlate that = (LicensePlate) object;
+		return Objects.equals(liNumber, that.liNumber) && Objects.equals(country, that.country);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(liNumber, country);
+	}
+
+	@Override
+	public String toString() {
+		return country + " " + liNumber;
+	}
 
 }
