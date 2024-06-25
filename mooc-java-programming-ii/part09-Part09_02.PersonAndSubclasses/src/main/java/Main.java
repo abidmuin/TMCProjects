688d2ecd49c40e267a5d1a521a7816ec5e099824
Abid Hasan Muin <muin.739@gmail.com>
2024-06-25 10:40:23 +0600

    PersonAndSubclasses

--- mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Person> persons = new ArrayList<Person>();
+		ArrayList<Person> persons = new ArrayList<>();
 		persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
 		persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
--- mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Teacher.java
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Teacher.java
@@ -1,14 +1,9 @@
 public class Teacher extends Person {
 	private final int salary;
-	private int studyCredits;
 
 	public Teacher(String name, String address, int salary) {
 		super(name, address);
 		this.salary = salary;
-	}
-
-	public int getSalary() {
-		return salary;
 	}
 
 	@Override
