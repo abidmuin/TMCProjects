718061393c500b7322226ce669d4630c2f0f5738
Abid Hasan Muin <muin.739@gmail.com>
2024-07-01 06:37:50 +0600

    AverageOfSelectedNumbers

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,44 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Input numbers, type \"end\" to stop.");
+		List<Integer> numbers = new ArrayList<>();
 
-		int positiveCounter = 0;
-		int negativeCounter = 0;
-		int positiveSum = 0;
-		int negativeSum = 0;
-		double average;
+		System.out.println("Input numbers, type \"end\" to stop.");
 
 		while (true) {
 			String input = scanner.nextLine();
 
 			if (input.equals("end")) {
 				break;
-			} else {
-				int number = Integer.parseInt(input);
-
-				if (number >= 0) {
-					positiveSum += number;
-					positiveCounter++;
-				} else {
-					negativeSum += number;
-					negativeCounter++;
-				}
 			}
+
+			numbers.add(Integer.parseInt(input));
 		}
 
 		System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
 		String input = scanner.nextLine();
+		OptionalDouble average;
 
 		if (input.equals("p")) {
-			average = (double) positiveSum / positiveCounter;
-			System.out.println("Average of the positive numbers: " + average);
+			average = numbers.stream().mapToInt(x -> x).filter(x -> x >= 0).average();
+			if (average.isPresent()) {
+				System.out.println("Average of the positive numbers: " + average.getAsDouble());
+			}
 		} else if (input.equals("n")) {
-			average = (double) negativeSum / negativeCounter;
-			System.out.println("Average of the negative numbers: " + average);
+			average = numbers.stream().mapToInt(x -> x).filter(x -> x < 0).average();
+			if (average.isPresent()) {
+				System.out.println("Average of the negative numbers: " + average.getAsDouble());
+			}
 		}
 	}
 }
