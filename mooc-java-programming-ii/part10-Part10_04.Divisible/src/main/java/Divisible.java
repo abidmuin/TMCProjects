33eef291fc5bed61619ae6d6a6bf1f2bd2da6246
Abid Hasan Muin <muin.739@gmail.com>
2024-07-01 23:42:46 +0600

    Divisible

--- mooc-java-programming-ii/part10-Part10_04.Divisible/src/main/java/Divisible.java
+++ mooc-java-programming-ii/part10-Part10_04.Divisible/src/main/java/Divisible.java
@@ -1,25 +1,25 @@
-
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Divisible {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(3);
-        numbers.add(2);
-        numbers.add(-17);
-        numbers.add(-5);
-        numbers.add(7);
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(3);
+		numbers.add(2);
+		numbers.add(-17);
+		numbers.add(-5);
+		numbers.add(7);
 
-        ArrayList<Integer> divisible = divisible(numbers);
+		ArrayList<Integer> divisible = divisible(numbers);
 
-        divisible.stream()
-                .forEach(luku -> System.out.println(luku));
-    }
+		divisible.stream().forEach(luku -> System.out.println(luku));
+	}
 
-    public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
-        return new ArrayList<>();
-    }
+	public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
+		List<Integer> divisibleNumbers = numbers.stream().filter(x -> x % 2 == 0 || x % 3 == 0 || x % 5 == 0).collect(Collectors.toList());
 
+		return new ArrayList<>(divisibleNumbers);
+	}
 }
