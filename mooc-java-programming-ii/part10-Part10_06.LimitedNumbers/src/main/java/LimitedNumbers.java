3a81c4fed91d83142fcb1d2ae00bf5e03fc12217
Abid Hasan Muin <muin.739@gmail.com>
2024-07-02 07:42:23 +0600

    LimitedNumbers

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,26 @@
-
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LimitedNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		List<Integer> limitedNumbers = new ArrayList<>();
 
-    }
+		while (scanner.hasNextInt()) {
+			int n = scanner.nextInt();
+
+			if (n < 0) {
+				break;
+			}
+
+			limitedNumbers.add(n);
+		}
+
+		limitedNumbers = limitedNumbers.stream().filter(x -> x > 0 && x < 6).collect(Collectors.toList());
+
+		limitedNumbers.forEach(System.out::println);
+	}
 }
