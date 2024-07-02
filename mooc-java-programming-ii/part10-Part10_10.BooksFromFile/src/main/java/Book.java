f672a62eb6c718a0e22a8233881d14b7fd77c0a2
Abid Hasan Muin <muin.739@gmail.com>
2024-07-02 11:54:02 +0600

    BooksFromFile

--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/Book.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/Book.java
@@ -1,32 +1,29 @@
-
 public class Book {
+	private final String name;
+	private final int publishingYear;
+	private final int pageCount;
+	private final String author;
 
-    private String name;
-    private int publishingYear;
-    private int pageCount;
-    private String author;
+	public Book(String name, int ReleaseYear, int pages, String author) {
+		this.name = name;
+		this.publishingYear = ReleaseYear;
+		this.pageCount = pages;
+		this.author = author;
+	}
 
-    public Book(String name, int ReleaseYear, int pages, String author) {
-        this.name = name;
-        this.publishingYear = ReleaseYear;
-        this.pageCount = pages;
-        this.author = author;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public int getPublishingYear() {
+		return publishingYear;
+	}
 
-    public int getPublishingYear() {
-        return publishingYear;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPagecount() {
-        return pageCount;
-    }
-
+	public int getPagecount() {
+		return pageCount;
+	}
 }
--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -1,17 +1,27 @@
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // test your method here
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// test your method here
 
-    }
+	}
 
+	public static List<Book> readBooks(String file) {
+		List<Book> books = new ArrayList<>();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			return reader.lines().map(line -> line.split(",")).filter(parts -> parts.length == 4).map(parts -> new Book(parts[0].trim(), Integer.parseInt(parts[1].trim()), Integer.parseInt(parts[2].trim()), parts[3].trim())).collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		return books;
+	}
 }
