9489baa7803ba1107d78182d7d3aa24e239ec619
Abid Hasan Muin <muin.739@gmail.com>
2024-07-02 09:28:57 +0600

    ReadingFilesPerLine

--- mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -1,17 +1,28 @@
-
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReadingFilesPerLine {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // test the method here
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// test the method here
 
-    }
+	}
 
+	public static List<String> read(String file) {
+		List<String> data = new ArrayList<>();
+
+		try (Stream<String> lines = Files.lines(Paths.get(file))) {
+			lines.forEach(data::add);
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return data;
+	}
 }
