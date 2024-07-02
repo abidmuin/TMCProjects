46132f67e293b85cc057c6bfd3276a1a47d64cf3
Abid Hasan Muin <muin.739@gmail.com>
2024-07-02 07:34:34 +0600

    PrintingUserInput

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,13 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PrintingUserInput {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    }
+		while (scanner.hasNextLine()) {
+			String input = scanner.nextLine();
+			System.out.println(input);
+		}
+	}
 }
