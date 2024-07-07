d0823b8a8c3907b39e74818104de006e0bae6322
Abid Hasan Muin <muin.739@gmail.com>
2024-07-07 10:48:10 +0600

    RegularExpressions

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,16 @@
-
-
 public class Checker {
+	public boolean isDayOfWeek(String string) {
+		String regexPattern = "sat|sun|mon|tue|wed|thu|fri";
+		return string.matches(regexPattern);
+	}
 
+	public boolean allVowels(String string) {
+		String regexPattern = "^[aeiou]+$";
+		return string.matches(regexPattern);
+	}
+
+	public boolean timeOfDay(String string) {
+		String regexPattern = "([01]\\d|2[0-3]):([0-5]\\d):([0-5]\\d)";
+		return string.matches(regexPattern);
+	}
 }
