03f0d73a7ae26718e4a8b3073dcf2caa8d413e65
Abid Hasan Muin <muin.739@gmail.com>
2024-07-09 14:33:34 +0600

    ValidatingParameters

--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -2,21 +2,30 @@ package validating;
 
 public class Calculator {
 
-    public int factorial(int num) {
+	public int factorial(int num) {
+		if (num < 0) {
+			throw new IllegalArgumentException("Invalid number");
+		}
 
-        int answer = 1;
-        for (int i = 1; i <= num; i++) {
-            answer *= i;
-        }
+		int answer = 1;
+		for (int i = 1; i <= num; i++) {
+			answer *= i;
+		}
 
-        return answer;
-    }
+		return answer;
+	}
 
-    public int binomialCoefficent(int setSize, int subsetSize) {
+	public int binomialCoefficent(int setSize, int subsetSize) {
+		if (setSize < 0 || subsetSize < 0) {
+			throw new IllegalArgumentException("Invalid size");
+		}
+		if (subsetSize > setSize) {
+			throw new IllegalArgumentException("Invalid subset size");
+		}
 
-        int numerator = factorial(setSize);
-        int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
+		int numerator = factorial(setSize);
+		int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
 
-        return numerator / denominator;
-    }
+		return numerator / denominator;
+	}
 }
--- mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ mooc-java-programming-ii/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -2,20 +2,27 @@ package validating;
 
 public class Person {
 
-    private String name;
-    private int age;
+	private final String name;
+	private final int age;
 
-    public Person(String name, int age) {
+	public Person(String name, int age) {
+		if (name == null || name.isEmpty() || name.length() > 40) {
+			throw new IllegalArgumentException("Name cannot be null or empty or less than 40 characters");
+		}
 
-        this.name = name;
-        this.age = age;
-    }
+		if (age < 0 || age > 120) {
+			throw new IllegalArgumentException("Age cannot be less than 0 or more than 120 years");
+		}
 
-    public String getName() {
-        return name;
-    }
+		this.name = name;
+		this.age = age;
+	}
 
-    public int getAge() {
-        return age;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
 }
