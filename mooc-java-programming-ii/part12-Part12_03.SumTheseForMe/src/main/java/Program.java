adf209e248d02e4a7ea1b8ad03c715747c52e8dc
Abid Hasan Muin <muin.739@gmail.com>
2024-07-11 10:33:47 +0600

    SumTheseForMe

--- mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,12 +1,32 @@
-
-import java.util.Scanner;
-
 public class Program {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // test your method here
+	public static void main(String[] args) {
+		int[] numbers = {3, -1, 8, 4};
 
-    }
+		System.out.println(sum(numbers, 0, 0, 0, 0));
+		System.out.println(sum(numbers, 0, 0, 0, 10));
+		System.out.println(sum(numbers, 0, 1, 0, 10));
+		System.out.println(sum(numbers, 0, 1, -10, 10));
+		System.out.println(sum(numbers, -1, 999, -10, 10));
+	}
 
+	public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+		if (fromWhere < 0) {
+			fromWhere = 0;
+		}
+
+		if (toWhere > array.length) {
+			toWhere = array.length;
+		}
+
+		int result = 0;
+
+		for (int i = fromWhere; i < toWhere; i++) {
+			if ((array[i] >= smallest) && (array[i] <= largest)) {
+				result += array[i];
+			}
+		}
+
+		return result;
+	}
 }
