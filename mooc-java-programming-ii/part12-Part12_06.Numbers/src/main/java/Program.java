2e0da0b9675d51a8e21c36adb1c4ebca28ee2b06
Abid Hasan Muin <muin.739@gmail.com>
2024-07-29 10:40:00 +0600

    Numbers

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -1,11 +1,18 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
+		System.out.println("How many random numbers should be printed?");
+		int count = scanner.nextInt();
 
+		Random random = new Random();
+
+		while (count > 0) {
+			System.out.println(random.nextInt(11));
+			count--;
+		}
+	}
 }
