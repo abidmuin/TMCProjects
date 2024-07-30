5efeb31f8b3a01c4bac073356dbc48afe61c9fb6
Abid Hasan Muin <muin.739@gmail.com>
2024-07-30 11:21:45 +0600

    Lottery

--- mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ mooc-java-programming-ii/part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -1,30 +1,46 @@
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class LotteryRow {
 
-    private ArrayList<Integer> numbers;
+	private final Random random = new Random();
+	private ArrayList<Integer> numbers;
 
-    public LotteryRow() {
-        // Draw the numbers when the LotteryRow is created
-        this.randomizeNumbers();
-    }
+	public LotteryRow() {
+		// Draw the numbers when the LotteryRow is created
+		this.randomizeNumbers();
+	}
 
-    public ArrayList<Integer> numbers() {
-        return this.numbers;
-    }
+	public ArrayList<Integer> numbers() {
+		return this.numbers;
+	}
 
-    public void randomizeNumbers() {
-        // Initialize the list for numbers
-        this.numbers = new ArrayList<>();
-        // Implement the random number generation here
-        // the method containsNumber is probably useful
-    }
+	public void randomizeNumbers() {
+		// Initialize the list for numbers
+		this.numbers = new ArrayList<>();
+		// Implement the random number generation here
+		// the method containsNumber is probably useful
 
-    public boolean containsNumber(int number) {
-        // Check here whether the number is among the drawn numbers
-        return false;
-    }
+		while (this.numbers.size() < 7) {
+			int randomNumber = this.random.nextInt(40) + 1;
+
+			if (!this.numbers.contains(randomNumber)) {
+				this.numbers.add(randomNumber);
+			}
+		}
+	}
+
+	public boolean containsNumber(int number) {
+		// Check here whether the number is among the drawn numbers
+		boolean found = false;
+
+		for (int value : numbers) {
+			if (value == number) {
+				found = true;
+				break;
+			}
+		}
+
+		return found;
+	}
 }
-
