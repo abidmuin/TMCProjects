e689667b693953140ef594480b3717c716570d03
Abid Hasan Muin <muin.739@gmail.com>
2024-07-30 11:48:15 +0600

    ArrayAsAString

--- mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -1,8 +1,21 @@
-
 public class Program {
 
-    public static void main(String[] args) {
-        // Test your method here
-    }
+	public static void main(String[] args) {
+		// Test your method here
+		int[][] matrix = {{3, 2, 7, 6}, {2, 4, 1, 0}, {3, 2, 1, 0}};
+		System.out.println(arrayAsString(matrix));
+	}
 
+	public static String arrayAsString(int[][] array) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (int[] rows : array) {
+			for (int value : rows) {
+				stringBuilder.append(value);
+			}
+			stringBuilder.append("\n");
+		}
+
+		return stringBuilder.toString();
+	}
 }
