c98610b7eb7c61e3ca244d5f5411e42fc9131b65
Abid Hasan Muin <muin.739@gmail.com>
2024-08-27 14:31:14 +0600

    MagicSquare

--- mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -1,12 +1,31 @@
-
 public class MagicSquareFactory {
 
-    public MagicSquare createMagicSquare(int size) {
+	public MagicSquare createMagicSquare(int size) {
 
-        MagicSquare square = new MagicSquare(size);
+		MagicSquare square = new MagicSquare(size);
 
-        // implement the creation of a magic square with the Siamese method algorithm here
-        return square;
-    }
+		// implement the creation of a magic square with the Siamese method algorithm here
+		int number = 1;
+		int row = 0;
+		int column = size / 2;
+		int totalSize = size * size;
 
+		while (number <= totalSize) {
+			square.placeValue(row, column, number);
+			number++;
+
+			// update row and column
+			int updatedRow = (row - 1 + size) % size;
+			int updatedColumn = (column + 1) % size;
+
+			if (square.readValue(updatedRow, updatedColumn) != 0) {
+				row = (row + 1) % size;
+			} else {
+				row = updatedRow;
+				column = updatedColumn;
+			}
+		}
+
+		return square;
+	}
 }
